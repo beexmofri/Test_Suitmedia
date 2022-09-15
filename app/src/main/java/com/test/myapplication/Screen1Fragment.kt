2c00e003cccc407b38a96f4f1617ab82b435adcb
Binar <binarmohamadfaruqirayono@gmail.com>
2022-09-15 09:33:42 +0700

    First commit

---
+++ app/src/main/java/com/test/myapplication/Screen1Fragment.kt
@@ -0,0 +1,26 @@
+package com.test.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.test.myapplication.databinding.FragmentScreen1Binding
+
+class Screen1Fragment : Fragment() {
+    private var _binding: FragmentScreen1Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentScreen1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
